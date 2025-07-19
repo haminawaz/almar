6308214d1bd0c0c263a6e8d3cbc533ac909ede65
hnawazatcodezac <hamid.nawaz@codezac.com>
2025-07-20 02:25:41 +0500

    ALMAR - updated vidoe in the hero section

--- components/HeroSection.tsx
+++ components/HeroSection.tsx
@@ -4,10 +4,18 @@ export default function HeroSection() {
   return (
     <section
       className="relative h-screen flex items-center justify-center overflow-hidden bg-cover bg-center bg-no-repeat"
-      style={{
-        backgroundImage: `url('/images/hero.png')`,
-      }}
     >
+      <video
+        autoPlay
+        loop
+        muted
+        playsInline
+        className="absolute top-0 left-0 w-full h-full object-cover z-[-1]"
+      >
+        <source src="/videos/hero.mp4" type="video/mp4" />
+        Your browser does not support the video tag.
+      </video>
+
       <div className="absolute bottom-10 left-5 md:left-10 z-10 text-white px-4">
         <h1 className="text-xl sm:text-3xl lg:4xl xl:text-5xl font-bold mb-8">
           ALMAR - Powering Precision.
@@ -19,9 +27,7 @@ export default function HeroSection() {
           and air, all at the tactical edge.
         </p>
         <button className="group bg-transparent text-white py-2 rounded-sm font-medium text-xl md:text-2xl xl:text-3xl inline-flex items-center">
-          <span className="mr-3">
-            Learn more
-          </span>
+          <span className="mr-3">Learn more</span>
           <span className="block group-hover:hidden w-8 h-8 md:w-9 md:h-9 xl:w-10 xl:h-10">
             <Plus className="w-full h-full" />
           </span>
